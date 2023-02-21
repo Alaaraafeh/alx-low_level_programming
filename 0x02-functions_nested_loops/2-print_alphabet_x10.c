e91f142cd021a0536d6aa6bbfd9e26d830b2d0f1
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-21 10:53:12 -0800

    task_2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * 2-print_alphabet_x10.c - check the code.
  *
  * return: always 0.
  */
-void main(void)
+void 2-print_alphabet_x10.c(void)
 {
 	for (int i = 0; i < 10; i++)
 	{
