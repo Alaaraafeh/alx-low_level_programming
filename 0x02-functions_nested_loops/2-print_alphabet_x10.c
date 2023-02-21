ffc8d8841c0057b1e4f00951f2dc9469020e3387
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-21 06:38:04 -0800

    task_2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * return: always 0.
+ */
+void main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (char c = 'a'; c <= 'z'; c++)
+		{
+			_putchar('c');
+			_putchar('\n');
+
+		}
