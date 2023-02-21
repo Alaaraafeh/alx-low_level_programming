8e26484007489b944f74eeb362fde83e0114ce33
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-21 06:27:13 -0800

    task_1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * main - check the code
+ *
+ * return: always 0.
+ */
+
+void main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
