1e5a8576377bfc743f1476c548c7a36c0abe925e
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-21 11:58:10 -0800

    task_4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 
 /**
  * _isalpha - check the code.
- * c is parameter
+ *@c: is parameter
  * Return: Always 0.
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
