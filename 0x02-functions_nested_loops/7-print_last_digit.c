49619e8d0329a810b0fbbcb3e6c8addda4de8a53
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-21 13:04:14 -0800

    task_7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @n: parameter
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n / 10;
+	return (x);
+}
