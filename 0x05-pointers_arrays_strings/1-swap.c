214b0fb47c9f73143cfbb5d91d1943af1ed6c5d3
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-27 10:13:28 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ * @a: parameter 1
+ * @b: parameter 2
+ * return: always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int *x;
+	*x = *a;
+	*a = *b;
+	*b = *x;
+}
