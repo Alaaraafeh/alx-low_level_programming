7249ad850b5619032a4e77a0fb5f024f69dbaaa8
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-27 11:26:54 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: function parameter
+ * return: always 0.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		_putchar(str[i++]);
+	_putchar('\n');
