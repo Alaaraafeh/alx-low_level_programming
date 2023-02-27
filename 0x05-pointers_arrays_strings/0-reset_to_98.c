11615fc0ef362a0f7c065d8b15a6d2475ca636a5
Alaaraafeh <alaaraafeh@gmail.com>
2023-02-27 09:49:30 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * reset_to_98 - check the code
+ *
+ *@n: is a glopal variable
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n){
+void reset_to_98(int *n)
+{
 	n = 98;
 }
