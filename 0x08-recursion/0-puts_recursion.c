46cfab2ba37833453800b9cf367e559db13fb264
Alaaraafeh <alaaraafeh@gmail.com>
2023-03-09 06:43:29 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,17 @@
 
 /**
  * _puts_recursion - check the code
- * *s: parameter that pointer to an array of char
+ * *@s: parameter that pointer to an array of char
  * return: always 0
  */
 
-void _puts_recursion(char *s){
-	if(*s)
+void _puts_recursion(char *s)
+{
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-		_putchar("\n");
+		_putchar('\n');
 }
